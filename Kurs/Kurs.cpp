a0d88d8f17ef6ca512209508bc4d04972bc8d2f7
a <a@b.com>
2021-01-20 21:55:59 +0300

    Буфер вершин заменён на буфер цветов

--- Kurs/Kurs.cpp
+++ Kurs/Kurs.cpp
@@ -14,10 +14,7 @@ using namespace std;
 
 int* GraphColoring(int** Matrix, const unsigned int size)
 {
-	struct v {
-		unsigned int n; // номер вершины
-		unsigned int color; // цвет вершины, 0 - не окрашена
-	};
+
 	int* ColorArray = NULL; // возвращаемый массив цветов, индексы - номера вершин, значения - присвоенный цвет
 	unsigned int r = 1; // счётчик цветов
 	if( !(ColorArray = (int*)calloc(size, sizeof(int))) )
@@ -26,7 +23,7 @@ int* GraphColoring(int** Matrix, const unsigned int size)
 	ColorArray[0] = 1;
 	/*
 	Проходим по матрице смежности и смотрим, есть ли из i-ой вершины путь в k-ю, после чего составляем
-	локальный массив-буфер, в который записываем номера и цвета всех смежных i-ой вершин, после чего красим
+	локальный массив-буфер, в который записываем цвета всех смежных i-ой вершин, после чего красим
 	текущую в минимальный доступный цвет и заносим этот цвет в ColorArray.
 	   В цикле:
 			i - вершина, которую нужно покрасить;
@@ -34,24 +31,23 @@ int* GraphColoring(int** Matrix, const unsigned int size)
 
 	*/
 	for(unsigned int i = 1; i < size; i++) {
-		v b = {0}; // буферная вершина
-		vector<v> buf; // локальный вектор-буфер
+		vector<int> buf; // локальный вектор-буфер
 
 		/* Каждую итерацию этот цикл составляет вектор всех вершин, смежных i-ой */
 		for(unsigned int k = 0; k < size; k++) {
 			if(Matrix[i][k]) {
-				b.n = k;
-				b.color = ColorArray[k];
-				buf.push_back(b);
+				buf.push_back(ColorArray[k]);
 			}
 		}
+
 		if(buf.size()) { // если размер буфера равен 0, то вершина изолирована
 			
 			/* Сортируем буфер по возрастанию цветов */
 			while (1) { // проходим по массиву, пока не отсортирован
+				int b = 0;
 				bool operated = false; // показывает, была ли проведена операция над массивом
 				for(unsigned int j = 1; j < buf.size(); j++) {
-					if (buf[j].color < buf[j - 1].color) {
+					if (buf[j] < buf[j - 1]) {
 						b = buf[j];
 						buf[j] = buf[j - 1];
 						buf[j - 1] = b;
@@ -61,26 +57,18 @@ int* GraphColoring(int** Matrix, const unsigned int size)
 				if(!operated) break;
 			}
 
-			printf("\nSorted buf:\n");
-			for(int j = 0; j < buf.size(); j++) {
-				printf("%d ", buf[j].n);
-			}
-			printf("\n\n");
-			for(int j = 0; j < buf.size(); j++) {
-				printf("%d ", buf[j].color);
-			}
 			/* Этот цикл находит минимальный неиспользованный цвет в буфере */
-			int CurrentColor = 0; // Собственно искомый цвет
+			int TargetColor = 0; // Собственно искомый цвет
 
 			for(int j = 1; j < buf.size(); j++) {
-				if((buf[j].color - buf[j - 1].color) >= 2) {
-					CurrentColor = buf[j - 1].color + 1;
+				if((buf[j] - buf[j - 1]) >= 2) {
+					TargetColor = buf[j - 1]  + 1;
 					break;
 				}
 			}
-			if(!CurrentColor) 
-				CurrentColor = ++r;
-			ColorArray[i] = CurrentColor;
+			if(!TargetColor) // если такого цвета нет в буфере, создаём новый цвет и красим в него i-ю вершину
+				TargetColor = ++r;
+			ColorArray[i] = TargetColor;
 			}
 		else {
 			ColorArray[i] = 1; // красим все изолированные вершины в цвет 1
